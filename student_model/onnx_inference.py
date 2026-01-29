b94f1aebcb6fc8a16f509424b925415faf92ee95
HajarHAMDOUCH01 <hajar.hamdouch02@gmail.com>
2026-01-29 19:16:06 +0100

    onnx inference

--- student_model/onnx_inference.py
+++ student_model/onnx_inference.py
@@ -2,12 +2,12 @@ import onnxruntime as ort
 import numpy as np
 from PIL import Image
 from torchvision import transforms
+import time
 
 # Load ONNX model
 session = ort.InferenceSession('/kaggle/working/tinydefectnet_student/checkpoints/student_model.onnx')
 
 class_names = ['crazing', 'inclusion', 'patches', 'pitted_surface', 'rolled-in_scale', 'scratches']
-
 
 # Preprocess
 transform = transforms.Compose([
@@ -19,8 +19,13 @@ transform = transforms.Compose([
 image = Image.open('/kaggle/input/neu-metal-surface-defects-data/NEU Metal Surface Defects Data/test/Crazing/Cr_1.bmp').convert('RGB')
 image_tensor = transform(image).unsqueeze(0).numpy()
 
-# Predict
+# Time the inference
+start_time = time.perf_counter()
 outputs = session.run(None, {'input': image_tensor})
+end_time = time.perf_counter()
+
+inference_time_ms = (end_time - start_time) * 1000
+
 logits = outputs[0][0]
 
 # Get prediction
@@ -29,3 +34,4 @@ pred_idx = np.argmax(probs)
 
 print(f"Prediction: {class_names[pred_idx]}")
 print(f"Confidence: {probs[pred_idx]:.2%}")
+print(f"Inference time: {inference_time_ms:.2f} ms")
